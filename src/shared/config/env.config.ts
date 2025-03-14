ce291ce3806233655a275320fcc617559c04c2c0
erickaguila <erick.aguila55@gmail.com>
2025-03-14 19:01:20 -0300

    Update env

--- src/shared/config/env.config.ts
+++ src/shared/config/env.config.ts
@@ -3,7 +3,19 @@ dotenv.config();
 
 export const configVar = {
   PORT: Number(process.env.PORT),
-  FIREBASE_CREDENTIALS: process.env.FIREBASE_CREDENTIALS,
+  FIREBASE: {
+    TYPE: process.env.FIREBASE_TYPE,
+    PROJECT_ID: process.env.FIREBASE_PROJECT_ID,
+    PRIVATE_KEY_ID: process.env.FIREBASE_PRIVATE_KEY_ID,
+    PRIVATE_KEY: process.env.FIREBASE_PRIVATE_KEY,
+    CLIENT_EMAIL: process.env.FIREBASE_CLIENT_EMAIL,
+    CLIENT_ID: process.env.FIREBASE_CLIENT_ID,
+    AUTH_URI: process.env.FIREBASE_AUTH_URI,
+    TOKEN_URI: process.env.FIREBASE_TOKEN_URI,
+    AUTH_PROVIDER_X509_CERT_URL: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+    CLIENT_X509_CERT_URL: process.env.FIREBASE_CLIENT_X509_CERT_URL,
+    UNIVERSE_DOMAIN: process.env.FIREBASE_UNIVERSE_DOMAIN,
+  },
   DATABASE: {
     HOST: process.env.DB_HOST,
     PORT: Number(process.env.DB_PORT),
--- src/shared/config/firebase.config.ts
+++ src/shared/config/firebase.config.ts
@@ -1,7 +1,20 @@
 import * as admin from 'firebase-admin';
 import { configVar } from './env.config';
 
-const firebaseConfig = JSON.parse(configVar.FIREBASE_CREDENTIALS);
+const firebaseConfigString = `{
+  "type": "${configVar.FIREBASE.TYPE}",
+  "project_id": "${configVar.FIREBASE.PROJECT_ID}",
+  "private_key_id": "${configVar.FIREBASE.PRIVATE_KEY_ID}",
+  "private_key": "${configVar.FIREBASE.PRIVATE_KEY}",
+  "client_email": "${configVar.FIREBASE.CLIENT_EMAIL}",
+  "client_id": "${configVar.FIREBASE.CLIENT_ID}",
+  "auth_uri": "${configVar.FIREBASE.AUTH_URI}",
+  "token_uri": "${configVar.FIREBASE.TOKEN_URI}",
+  "auth_provider_x509_cert_url": "${configVar.FIREBASE.AUTH_PROVIDER_X509_CERT_URL}",
+  "client_x509_cert_url": "${configVar.FIREBASE.CLIENT_X509_CERT_URL}",
+  "universe_domain": "${configVar.FIREBASE.UNIVERSE_DOMAIN}"
+}`;
+const firebaseConfig = JSON.parse(firebaseConfigString);
 
 if (!admin.apps.length) {
   admin.initializeApp({
